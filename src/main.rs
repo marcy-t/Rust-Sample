a5a8b1c3866dceb098de05d9adea7a002f8f7603
makito-tashiro <marcy.workout.life@gmail.com>
2020-04-18 02:27:30 +0900

    thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    thread::spawn(|| {
+        println!("Hello from a new thread!");
+    });
+
+    println!("Hello from a main thread!");
+    thread::sleep(Duration::from_millis(20));
+}
+
+/*
 fn main() {
     println!("Hello, world!");
+    println!("HOGEHOGEHOGE");
 }
+*/
+
